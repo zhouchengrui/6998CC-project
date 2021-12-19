bcda91788cad41d2bf7167e93e828bda1a74cb39
Ruoyu-Roy <rl3161@columbia.edu>
2021-12-19 11:15:03 -0500

    ruoyu2

---
+++ assets/js/login.js
@@ -0,0 +1,38 @@
+function signInButton() {
+    var authenticationData = {
+        Username : document.getElementById("Uname").value,
+        Password : document.getElementById("Pass").value,
+    };
+
+    var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
+
+    console.log(authenticationDetails);
+    var poolData = {
+        UserPoolId : "us-west-2_S8ChSRRDn",
+        ClientId : "58d1m8peeli9ehkfbtv1sqaga2"
+    };
+
+    var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+
+    console.log(userPool);
+    var userData = {
+        Username : document.getElementById("Uname").value,
+        Pool : userPool
+    };
+
+    var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+    console.log(cognitoUser);
+    cognitoUser.authenticateUser(authenticationDetails, {
+        onSuccess: function (result) {
+            var accessToken = result.getAccessToken().getJwtToken();
+            console.log(accessToken);
+            sessionStorage.setItem("username", document.getElementById("Uname").value);
+        },
+
+        onFailure: function (err) {
+            alert(err.message || JSON.stringify(err));
+        }
+    });
+
+    console.log(sessionStorage.getItem("username"));
+};
---
+++ assets/js/signup.js
@@ -0,0 +1,50 @@
+var username;
+var email;
+var password;
+var poolData;
+
+function signUpButton() {
+    username = document.getElementById("Uname").value;
+    email = document.getElementById("Email").value;
+    password = document.getElementById("Pass").value;
+
+    poolData = {
+        UserPoolId : "us-west-2_S8ChSRRDn",
+        ClientId : "58d1m8peeli9ehkfbtv1sqaga2"
+    };
+
+    var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+
+    var attributeList = [];
+
+    var dataEmail = {
+        Name: 'email',
+        Value: email
+    };
+
+    var dataUserName = {
+        Name: 'name',
+        Value: username
+    }
+
+    var attributeEmail = new AmazonCognitoIdentity.CognitoUserAttribute(dataEmail);
+    var attributeUserName = new AmazonCognitoIdentity.CognitoUserAttribute(dataUserName);
+
+    attributeList.push(attributeEmail);
+    attributeList.push(attributeUserName);
+
+    userPool.signUp(username, password, attributeList, null, function (err, result) {
+        if (err) {
+            alert(err.message || JSON.stringify(err));
+            return;
+        }
+        cognitoUser = result.user;
+        console.log("username is " +  cognitoUser.getUsername());
+        // document.getElementById("titleheader").innerHTML = "Check your email verification code"
+
+        // var registerfields_list = document.getElementById("registerfields");
+        // for (var i=0; i < registerfields.length; i++) {
+        //     registerfields[i].type = "hidden";
+        // }
+    });
+}
